088b024d82708d0efb7d7ea2704a3df24bfe9efe
ivan minutillo <minutillo.ivan@gmail.com>
2018-11-21 17:23:07 +0100

    balance sheet ui

--- src/ui/src/pages/agent/index.js
+++ src/ui/src/pages/agent/index.js
@@ -5,9 +5,11 @@ import media from "styled-media-query";
 import LogEvent from "../../logEvent";
 import Feed from "../../FeedItem";
 import moment from "moment";
+import {clearFix} from 'polished'
+import { ResponsivePie } from "@nivo/pie";
+import { ResponsiveLine } from '@nivo/line'
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-
 
 export default props => {
   return (
@@ -18,28 +20,154 @@ export default props => {
           <Inside>
             <Overview>
               <Tabs>
-              <StyledTabList>
-                <StyledTab>Exchange</StyledTab>
-                <StyledTab>Balance Sheet</StyledTab>
-              </StyledTabList>
+                <StyledTabList>
+                  <StyledTab>Exchange</StyledTab>
+                  <StyledTab>Balance Sheet</StyledTab>
+                </StyledTabList>
                 <TabPanel>
-                <SmartSentence>
-                  <LogEvent />
-                </SmartSentence>
-                <Tagline>Feed</Tagline>
-                <Feed
-                  primary={
-                    <FeedItem>
-                      <B>Ivan Minutillo</B>{" "}
-                      {"exchange" + " " + 5 + " " + "each of"}
-                      <i>{"pies"}</i>
-                    </FeedItem>
-                  }
-                  secondary={"Friendly gift"}
-                  date={moment().format("DD MMM")}
-                />
+                  <SmartSentence>
+                    <LogEvent />
+                  </SmartSentence>
+                  <Tagline>Feed</Tagline>
+                  <Feed
+                    primary={
+                      <FeedItem>
+                        <B>Ivan Minutillo</B>{" "}
+                        {"exchange" + " " + 5 + " " + "each of"}
+                        <i>{"pies"}</i>
+                      </FeedItem>
+                    }
+                    secondary={"Friendly gift"}
+                    date={moment().format("DD MMM")}
+                  />
                 </TabPanel>
-                <TabPanel>test</TabPanel>
+                <TabPanel>
+                  <BalanceContainer>
+                    <Assets>
+                      <Box>
+                        <BoxTitle>Total current assets</BoxTitle>
+                        <BoxData>
+                          <span>$</span>183.000
+                        </BoxData>
+                      </Box>
+                      <WrapperViz>
+                        <ResponsivePie
+                          margin={{
+                            top: 20,
+                            right: 40,
+                            bottom: 20,
+                            left: 40
+                          }}
+                          data={[
+                            {
+                              id: "lisp",
+                              label: "lisp",
+                              value: 391
+                            },
+                            {
+                              id: "stylus",
+                              label: "stylus",
+                              value: 512
+                            },
+                            {
+                              id: "php",
+                              label: "php",
+                              value: 45
+                            },
+                            {
+                              id: "make",
+                              label: "make",
+                              value: 225
+                            },
+                            {
+                              id: "go",
+                              label: "go",
+                              value: 11
+                            },
+                            {
+                              id: "c",
+                              label: "c",
+                              value: 373
+                            },
+                            {
+                              id: "javascript",
+                              label: "javascript",
+                              value: 440
+                            },
+                            {
+                              id: "python",
+                              label: "python",
+                              value: 536
+                            },
+                            {
+                              id: "java",
+                              label: "java",
+                              value: 232
+                            }
+                          ]}
+                          animate
+                          innerRadius={0.6}
+                        />
+                      </WrapperViz>
+                      <SubTitle>Current Assets</SubTitle>
+                      <Item active>Cash <span>123.00</span></Item>
+                      <Item active>Inventory <span>123.00</span></Item>
+                      <Item active>Account Receivable <span>123.00</span></Item>
+                    </Assets>
+                    <Liabilities>
+                      <Box>
+                        <BoxTitle>Total current assets</BoxTitle>
+                        <BoxData>
+                          <span>$</span>183.000
+                        </BoxData>
+                      </Box>
+                      <WrapperViz>
+                        <ResponsiveLine
+                          margin={{
+                            top: 20,
+                            right: 20,
+                            bottom: 40,
+                            left: 40
+                          }}
+                          data={[
+                            {
+                              id: "positive :)",
+                              data: [
+                                { x: 0, y: 0.7 },
+                                { x: 1, y: 0.9 },
+                                { x: 2, y: 0.8 },
+                                { x: 3, y: 0.8 },
+                                { x: 4, y: 0.9 },
+                                { x: 6, y: 0.4 },
+                              ]
+                            }
+                          ]}
+                          animate
+                          curve="monotoneX"
+                          enableDotLabel
+                          dotSymbol={CustomSymbol}
+                          dotSize={14}
+                          dotBorderWidth={1}
+                          dotBorderColor="inherit:darker(0.3)"
+                          dotLabelYOffset={-20}
+                          enableGridX={false}
+                          colors={["rgb(97, 205, 187)", "rgb(244, 117, 96)"]}
+                          xScale={{ type: "linear" }}
+                          yScale={{
+                            type: "linear",
+                            stacked: false,
+                            min: 0,
+                            max: 1
+                          }}
+                          enableArea
+                          areaOpacity={0.07}
+                        />
+                      </WrapperViz>
+                      <SubTitle>Current Assets</SubTitle>
+                      <Item>Account Payable <span>123.00</span></Item>
+                    </Liabilities>
+                  </BalanceContainer>
+                </TabPanel>
               </Tabs>
             </Overview>
           </Inside>
@@ -50,16 +178,96 @@ export default props => {
 };
 
 
+const CustomSymbol = ({ size, color, borderWidth, borderColor }) => (
+  <g>
+      <circle fill="#fff" r={size / 2} strokeWidth={borderWidth} stroke={borderColor} />
+      <circle
+          r={size / 5}
+          strokeWidth={borderWidth}
+          stroke={borderColor}
+          fill={color}
+          fillOpacity={0.35}
+      />
+  </g>
+)
+
+const SubTitle = styled.div`
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+  font-weight: 500;
+  margin-top: 24px;
+`
+
+const Item = styled.div`
+  height: 50px;
+  border-radius: 3px;
+  background: #353940;
+  line-height: 50px;
+  font-size: 14px;
+  color: #f0f0f0;
+  letter-spacing: 0.5px;
+  margin-top: 10px;
+  padding: 0 10px;
+  ${clearFix()};
+  ${props =>
+    props.active &&
+    css`
+      & span {
+        color: #26ED8E !important;
+      }
+    `};
+  & span {
+    float: right;
+    color: #EF2978;
+    font-size: 11px;
+    font-weight: 500;
+  }
+`
+
+const WrapperViz = styled.div`
+  background: #35394020;
+  border-radius: 3px;
+  padding: 4px;
+  margin-top: 20px;
+  height: 300px;
+`;
+
+const BalanceContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 24px;
+  margin-top: 10px;
+  padding: 0 10px;
+`;
+
+const Assets = styled.div``;
+const Liabilities = styled.div``;
+
+const Box = styled.div`
+  margin-top: 10px;
+`;
+const BoxTitle = styled.h3`
+  color: #677b83;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+`;
+const BoxData = styled.h2`
+  margin-top: 10px;
+  font-size: 26px;
+  letter-spacing: 1px;
+  color: #36393f;
+  & span {
+    color: #677b83;
+  }
+`;
+
 const StyledTabList = styled(TabList)`
   border-bottom: 4px solid #485c7a42;
   margin: 10px;
   padding: 0;
   border-radius: 2px;
-  
-`;
-
-const StyledTabPanel = styled(TabPanel)`
-  margin-top: 32px;
 `;
 
 const StyledTab = styled(Tab)`
@@ -95,7 +303,6 @@ const StyledTab = styled(Tab)`
       }
     `};
 `;
-
 
 const B = styled.b`
   font-weight: 500;
